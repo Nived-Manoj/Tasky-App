dbf398141f65c1ade5502ce9c51f94188ae3e686
Nived-Manoj <nivedmanoj001@gmail.com>
2023-12-29 16:43:29 +0530

    initial commit

--- lib/utils/color_constant.dart
+++ lib/utils/color_constant.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorConstant {
   static Color primaryRed = Color(0xFFFF3A44);
   static Color primaryBlue = Colors.blueAccent;
+  static Color Black = Colors.black;
 }
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mainproject_tasky/utils/color_constant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: Image.asset(
+          'assets/icons/Group(1).png',
+          color: ColorConstant.Black,
+        ),
+      ),
     );
   }
 }
