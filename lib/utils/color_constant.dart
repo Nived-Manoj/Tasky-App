8df2c6aade2393767ab9628adf74d631606a4d4e
Nived-Manoj <nivedmanoj001@gmail.com>
2023-12-29 16:16:46 +0530

    initial commit

---
+++ lib/utils/color_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static Color primaryRed = Color(0xFFFF3A44);
+  static Color primaryBlue = Colors.blueAccent;
+}
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -6,68 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            actions: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                  ),
-                  SizedBox(
-                    width: 300,
-                  ),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
-                ],
-              )
-            ],
-            stretch: true,
-            toolbarHeight: 100,
-            expandedHeight: 300,
-            backgroundColor: Color(0xFFFF3A44),
-            floating: true,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              stretchModes: [StretchMode.zoomBackground],
-              title: Padding(
-                padding: EdgeInsets.only(left: 50),
-                child: Text('T A S K Y'),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              height: 400,
-              color: Colors.blueAccent,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 20,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              height: 400,
-              color: Colors.blueAccent,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 20,
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              height: 400,
-              color: Colors.blueAccent,
-            ),
-          ),
-        ],
-      ),
+      appBar: AppBar(),
     );
   }
 }
