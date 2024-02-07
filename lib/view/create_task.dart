319c3d7899a07f8daf56c023bc5049f77ff3998f
Nived-Manoj <nivedmanoj001@gmail.com>
2024-02-07 17:57:13 +0530

    initial committ

--- lib/view/create_task.dart
+++ lib/view/create_task.dart
@@ -11,7 +11,19 @@ class CreateTask extends StatelessWidget {
             style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
       ),
       body: Column(
-        children: [],
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 80,
+              width: 390,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
