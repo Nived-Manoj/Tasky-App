5e74a3aaefb998fcccc0993619dd4bdd49fb82ea
Nived-Manoj <nivedmanoj001@gmail.com>
2024-03-14 15:57:06 +0530

    initial committ

--- lib/view/profile.dart
+++ lib/view/profile.dart
@@ -8,10 +8,11 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(
-          style: ButtonStyle(iconSize: MaterialStatePropertyAll(30)),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
+        // leading: BackButton(
+        //   style: ButtonStyle(iconSize: MaterialStatePropertyAll(30)),
+        //   onPressed: () => Navigator.of(context).pop(),
+        // ),
+        title: Text("My profile"),
       ),
       body: Column(
         children: [
@@ -25,6 +26,11 @@ class Profile extends StatelessWidget {
                   color: ColorConstant.Black,
                   fontSize: 30,
                   fontWeight: FontWeight.w500)),
+          Text("shanewatson@gmail.com",
+              style: TextStyle(
+                color: ColorConstant.Black,
+                fontSize: 20,
+              )),
           SizedBox(
             height: 8,
           ),
@@ -41,34 +47,108 @@ class Profile extends StatelessWidget {
             height: 8,
           ),
           Container(
-            height: 65,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.blueGrey,
+            ),
             width: 400,
-            color: Colors.amber,
-            // child: Column(Text()),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Container(
-            height: 65,
-            width: 400,
-            color: Colors.amber,
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Container(
-            height: 65,
-            width: 400,
-            color: Colors.amber,
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Container(
-            height: 65,
-            width: 400,
-            color: Colors.amber,
+            child: Column(
+              children: [
+                Container(
+                  height: 65,
+                  width: 380,
+                  color: Colors.amber,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Name"),
+                          Text(
+                            "Shane whatson",
+                            style: TextStyle(
+                                color: ColorConstant.Black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 65,
+                  width: 380,
+                  color: Colors.amber,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Name"),
+                          Text(
+                            "Shane whatson",
+                            style: TextStyle(
+                                color: ColorConstant.Black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 65,
+                  width: 380,
+                  color: Colors.amber,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Phone"),
+                          Text(
+                            "+14 223 224 553",
+                            style: TextStyle(
+                                color: ColorConstant.Black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  height: 65,
+                  width: 380,
+                  color: Colors.amber,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Birthday"),
+                          Text(
+                            "11/11/1999",
+                            style: TextStyle(
+                                color: ColorConstant.Black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
