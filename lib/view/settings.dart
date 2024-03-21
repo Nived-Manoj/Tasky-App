306ed07b2a95551e33919bb40f414cf8c5e96127
Nived-Manoj <nivedmanoj001@gmail.com>
2024-03-21 21:35:13 +0530

    initial committ

--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -160,7 +160,7 @@ class Settings extends StatelessWidget {
                           "Help",
                           style: TextStyle(
                               color: ColorConstant.Black,
-                              fontSize: 36,
+                              fontSize: 26,
                               fontWeight: FontWeight.w500),
                         ),
                         Text("arrow"),
